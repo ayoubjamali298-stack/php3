6d5b9988ce3a5b07767a6fded655ff96a9e6fdb0
ayoubjamali298-stack <ayoubjamali298@gmail.com>
2026-04-20 16:08:00 +0200

    chapitre4

---
+++ chapitre4/connexion.php
@@ -0,0 +1,18 @@
+
+<?php
+$host = 'localhost';
+$dbname = 'test1';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie à la base $dbname";
+} catch (PDOException $e) {
+   
+    file_put_contents('erreurs.log', $e->getMessage(), FILE_APPEND);
+    echo "Une erreur est survenue. Contactez l'administrateur.";
+
+}
+
